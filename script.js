ceadcb775d3a818e5338fb406b8799300aa250c4
dusty-g <dustygalindo@gmail.com>
2017-03-29 13:49:42 -0700

    updates

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 $(document).ready(function(){
-    $("button").hover(function(){
-        $(".wrapper").css("background-image", "url('matrix.jpg')");
+    $(".magic").hover(function(){
+        var test = $(this).attr("id");
+        $(".wrapper").css("background-image", "url("+test+".jpg)");
     }, function(){
         $(".wrapper").css("background-image", "url('black.jpg')");
     }
     
     );
+    $("button").click(function(){
+        var different = $(this).attr("id");
+        console.log(different);
+        $(".wrapper").css("background-image", "url("+different+".jpg)");
+        $("button").css("display", "none");
+        $("button").attr("class", "no-hover");
+        $("h1").text("Choose Fighter");
+        $("h1").next().html("<select><option value='raphael'>Raphael</option><option value='raphael'>Donny</option><option value='raphael'>Mikey</option></select> <select><option value='raphael'>Raphael</option><option value='raphael'>Donny</option><option value='raphael'>Mikey</option></select> ");
+    })
+
 });
