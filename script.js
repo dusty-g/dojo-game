d248f9fdc51353397d41716dcaed35e07ead7f84
dusty-g <dustygalindo@gmail.com>
2017-03-29 14:22:33 -0700

    updates

--- script.js
+++ script.js
@@ -7,14 +7,29 @@ $(document).ready(function(){
     }
     
     );
-    $("button").click(function(){
+    $(".magic").click(function(){
         var different = $(this).attr("id");
         console.log(different);
         $(".wrapper").css("background-image", "url("+different+".jpg)");
         $("button").css("display", "none");
         $("button").attr("class", "no-hover");
         $("h1").text("Choose Fighter");
-        $("h1").next().html("<select><option value='raphael'>Raphael</option><option value='raphael'>Donny</option><option value='raphael'>Mikey</option></select> <select><option value='raphael'>Raphael</option><option value='raphael'>Donny</option><option value='raphael'>Mikey</option></select> ");
+        $("h1").next().html("<select id='first'><option value='raphael1'>Raphael</option><option value='donny1'>Donny</option><option value='mikey1'>Mikey</option></select> <select id='second' ><option value='raphael2'>Raphael</option><option value='donny2'>Donny</option><option value='mikey2'>Mikey</option></select> ");
+        $("#secret").css("display", "block");
+    });
+    $("#secret").click(function(){
+        $(".ninjas").css("display", "none");
+        var ninja1 = $("#first").val();
+        console.log(ninja1);
+        var ninja2 = $("#second").val();
+        console.log(ninja2);
+
+        $("#"+ninja1).css("display", "inline-block");
+        $("#"+ninja2).css("display", "inline-block");
+        $("#"+ninja2).css("margin-left", "200px");
+
+
     })
+
 
 });
